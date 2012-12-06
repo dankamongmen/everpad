4eea1d6c9b322cbad021453f3a0dc57582fb5c6a
nick black <nick.black@sprezzatech.com>
2012-12-06 04:28:28 -0500

    probably a better way to do this check according to PEP8

--- everpad/provider/daemon.py
+++ everpad/provider/daemon.py
@@ -16,8 +16,7 @@ import getpass
 import argparse
 
 
-de = os.environ.get('DESKTOP_SESSION')
-if de != "" and 'kde' in os.environ.get('DESKTOP_SESSION'):  # kde init qwidget for wallet access
+if os.environ.has_key('DESKTOP_SESSION') and 'kde' in os.environ.get('DESKTOP_SESSION'):  # kde init qwidget for wallet access
     from PySide.QtGui import QApplication
     App = QApplication
 else:
